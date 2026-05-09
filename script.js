1b1fefbada33fba8754c06942da7e062548dd918
dsnzbee <parthpsh5@gmail.com>
2026-05-09 19:15:49 +0530

    Refactor device orientation handling and movement logic

--- script.js
+++ script.js
@@ -3,25 +3,76 @@ const yoke = document.getElementById("yoke");
 const blue = document.getElementById("blue");
 const orange = document.getElementById("orange");
 const ladder = document.getElementById("ladder");
+const start = document.getElementById("start");
 
-document.addEventListener("mousemove", function(event){
-  let x = event.gamma / window.innerWidth;
-  let y = event.beta / window.innerHeight;
+let currentRoll = 0;
+let currentPitch = 0;
 
-  let roll = (x - 0.5) * 35;
-  let pitch = (0.5 - y) * 100;
+let rollVelocity = 0;
+let pitchVelocity = 0;
+
+const rollSensitivity = 0.4;
+const pitchSensitivity = 0.5;
+
+const springStrength = 0.08;
+const damping = 0.85;
+
+function handleOrientation(event){
+
+  let gamma = event.gamma || 0;
+  let beta = event.beta || 0;
+
+  let targetRoll = gamma * rollSensitivity;
+  let targetPitch = -beta * pitchSensitivity;
+
+  rollVelocity += (targetRoll - currentRoll) * springStrength;
+  pitchVelocity += (targetPitch - currentPitch) * springStrength;
+
+  rollVelocity *= damping;
+  pitchVelocity *= damping;
+
+  currentRoll += rollVelocity;
+  currentPitch += pitchVelocity;
 
   debug.innerHTML =
-    "X: " + Math.round(event.gamma) + "<br>" +
-    "Y: " + Math.round(event.beta) + "<br>" +
-    "ROLL: " + Math.round(roll) + " DEG<br>" +
-    "PITCH: " + Math.round(pitch);
+    "GAMMA: " + Math.round(gamma) + "<br>" +
+    "BETA: " + Math.round(beta) + "<br>" +
+    "ROLL: " + Math.round(currentRoll) + " DEG<br>" +
+    "PITCH: " + Math.round(currentPitch);
 
-  yoke.style.left = event.gamma + "px";
-  yoke.style.top = event.beta + "px";
+  let move =
+    "translateY(" + currentPitch + "px) rotate(" + currentRoll + "deg)";
 
-  let move = "translateY(" + pitch + "px) rotate(" + roll + "deg)";
   blue.style.transform = move;
   orange.style.transform = move;
   ladder.style.transform = move;
+}
+
+start.addEventListener("click", function(){
+
+  if(typeof DeviceOrientationEvent.requestPermission === "function"){
+
+    DeviceOrientationEvent.requestPermission()
+      .then(function(permission){
+
+        if(permission === "granted"){
+
+          window.addEventListener(
+            "deviceorientation",
+            handleOrientation
+          );
+
+        }
+
+      });
+
+  } else {
+
+    window.addEventListener(
+      "deviceorientation",
+      handleOrientation
+    );
+
+  }
+
 });
